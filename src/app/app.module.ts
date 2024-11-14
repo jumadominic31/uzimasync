bc2d2656e57dbbd2fa6157c0617dd396a630ea3d
DJuma <jumadominic@yahoo.com>
2024-11-14 05:28:20 +0200

    v0_141124

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { Health } from '@awesome-cordova-plugins/health/ngx';
+
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +13,10 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, 
+    Health, 
+    provideHttpClient(withInterceptorsFromDi())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Health } from '@awesome-cordova-plugins/health/ngx';
+import { Platform } from '@ionic/angular';
+import { HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,68 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(
+    private health: Health, 
+    private platform: Platform, 
+    private http: HttpClient) {
+    this.platform.ready().then(() => {
+      this.requestHealthAuthorization();
+    });
+  }
 
-  constructor() {}
+  requestHealthAuthorization() {
+    this.health.isAvailable().then((available) => {
+      if (available) {
+        // Request read permission for step count and heart rate
+        this.health.requestAuthorization([
+          {
+            read: ['steps', 'heart_rate', 'distance', 'calories'],
+          }
+        ]).then(res => console.log('HealthKit Authorized'), 
+                err => console.log('Error: ', err));
+      }
+    });
+  }
 
+  fetchHealthData() {
+    this.health.query({
+      startDate: new Date(new Date().setDate(new Date().getDate() - 1)), // yesterday
+      endDate: new Date(), // now
+      dataType: 'steps',
+      limit: 1000
+    }).then(data => {
+      console.log('Steps: ', data);
+    }, err => {
+      console.log('Error fetching steps: ', err);
+    });
+  }
+
+  fetchHeartRateData() {
+    this.health.query({
+      startDate: new Date(new Date().setDate(new Date().getDate() - 1)),
+      endDate: new Date(),
+      dataType: 'heart_rate',
+      limit: 1000
+    }).then(data => {
+      console.log('Heart Rate: ', data);
+    }, err => {
+      console.log('Error fetching heart rate: ', err);
+    });
+  }
+
+  exportDataToAPI(steps: any, heartRate: any) {
+    const data = {
+      steps: steps,
+      heartRate: heartRate
+    };
+
+    this.http.post('https://yourapi.com/export', data)
+      .subscribe(response => {
+        console.log('Data exported: ', response);
+      }, error => {
+        console.log('Error exporting data: ', error);
+      });
+      
+  }
+  
 }
